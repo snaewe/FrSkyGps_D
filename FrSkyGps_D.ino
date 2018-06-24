a1ba43f7155ef194f9b130188b6cc52e2688f022
Stefan Naewe <stefan.naewe@gmail.com>
2018-06-24 17:27:49 +0200

    initial commit
    Signed-off-by: Stefan Naewe <stefan.naewe@gmail.com>

---
+++ FrSkyGps_D.ino
@@ -0,0 +1,48 @@
+// U-Blox M8N on pins 8&9
+// FrSky RX on pin 3
+
+#include "config.h"
+
+#include <NMEAGPS.h>
+#include <GPSfix.h>
+#include <GPSport.h>
+
+#include <FrSkyTelemetry.h>
+
+static NMEAGPS gps;
+static gps_fix fix;
+static FrSkyTelemetry telemetry;
+
+void fillTelemetry(gps_fix& fix, FrSkyTelemetry& telemetry)
+{
+  telemetry.setGpsData(
+    fix.latitude(), fix.longitude(),
+    fix.altitude(),
+    fix.speed_kph() / 3.6,  // km/h -> m/sec
+    fix.heading(),
+    fix.dateTime.full_year(), fix.dateTime.month, fix.dateTime.date,
+    fix.dateTime.hours, fix.dateTime.minutes, fix.dateTime.seconds);
+  float t1 = 0.0, t2 = 0.0;
+  if (fix.valid.status)
+    t1 = (int(fix.status)+1000.0) + (fix.satellites*1.0);
+#ifdef GPS_FIX_HDOP
+  if (fix.valid.hdop)
+    t2 = fix.hdop;
+#endif
+  telemetry.setTemsData(t1, t2);
+  telemetry.send();
+}
+
+void setup()
+{
+  gpsPort.begin( 9600 );
+  telemetry.begin(FrSkyTelemetry::SOFT_SERIAL_PIN_3);
+}
+
+void loop()
+{
+  while (gps.available( gpsPort )) {
+    fix = gps.read();
+    fillTelemetry(fix, telemetry);
+  }
+}
---
+++ config.h
@@ -0,0 +1,7 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define GPS_FIX_HDOP
+#define GPS_FIX_VDOP
+
+#endif
